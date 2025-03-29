a473b0650c083060b0ae4944166f86dada5bf285
Jordan Rothwell <jrothwe3@myune.edu.au>
2025-03-29 16:16:54 +1000

    Initial project setup

---
+++ Main.scala
@@ -0,0 +1,13 @@
+//> using scala 3.3.1
+//> using dep org.scalafx:scalafx_3:21.0.0-R32
+//> using dep dev.zio::zio:2.1.16
+//> using dep org.typelevel::cats-effect:3.6.0
+//> using dep org.openjfx:javafx-base:24
+//> using dep org.openjfx:javafx-controls:24
+//> using dep org.openjfx:javafx-fxml:24
+//> using dep org.openjfx:javafx-graphics:24
+//> using dep org.openjfx:javafx-media:24
+//> using dep org.scalameta::munit:1.1.0
+
+// For now, let's use a simple main function until we create our app structure
+@main def hello = println("Hello, Wizard 500!")
